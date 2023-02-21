488b6d12408e9a11dfeb97470e76740cfc18f67f
Ramzi Jabali <ramzi@eljabali.org>
2023-02-21 16:19:09 -0700

    bare bones

---
+++ test1.cpp
@@ -0,0 +1,6 @@
+#include "program1.h"
+
+/* You may use this file to test your program.
+	This file will not be submitted, as a different test1.cpp will be used. */
+int main(int argv, char **argc) {
+}
